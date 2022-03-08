41551c479c29d352c101680780b5931c93616ad0
bhargavabhamidipati <bhargavabhamidipati@gmail.com>
2022-03-08 15:30:13 +0100

    Initial commit

---
+++ src/main/Calculator.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Calculator {
+    public static void main(String args[]){
+        System.out.println("hello world");
+    }
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int substract(int a, int b){
+        return a-b;
+    }
+    public int multiply(int a,int b){
+        return a*b;
+    }
+}
---
+++ src/test/Testsclass.java
@@ -0,0 +1,20 @@
+import main.Calculator;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Testsclass{
+
+
+    @Test
+    public void testAdd(){
+        Calculator obj;
+        obj = new Calculator();
+        assertEquals(4,obj.add(2,2));
+    }
+    @Test
+    public void testSub(){
+        Calculator obj;
+        obj = new Calculator();
+        assertEquals(0,obj.substract(2,2));
+    }
+}
