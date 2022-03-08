276c4429fdce400e7374135f5ef4719dae82553d
bhargavabhamidipati <bhargavabhamidipati@gmail.com>
2022-03-08 23:17:05 +0100

    CHANGED TESTCLASS

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -7,10 +7,5 @@ public class Calculator {
     public int add(int a, int b){
         return a+b;
     }
-    public int substract(int a, int b){
-        return a-b;
-    }
-    public int multiply(int a,int b){
-        return a*b;
-    }
+
 }
